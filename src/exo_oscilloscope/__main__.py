60292de8a608dfca5777918dc8594c3afe6768b4
tsmorz <tonysmoragiewicz@gmail.com>
2025-11-13 15:56:01 +0100

    update plotter to be bare bones

--- src/exo_oscilloscope/__main__.py
+++ src/exo_oscilloscope/__main__.py
@@ -2,11 +2,10 @@
 
 import argparse
 
-import numpy as np
-from PySide6 import QtCore
+from loguru import logger
 
 from exo_oscilloscope.config.definitions import DEFAULT_LOG_LEVEL, LogLevel
-from exo_oscilloscope.plotter import IMUOscilloscope
+from exo_oscilloscope.plotter import ExoPlotter
 from exo_oscilloscope.utils import setup_logger
 
 
@@ -20,36 +19,10 @@ def main(
     :return: None
     """
     setup_logger(log_level=log_level, stderr_level=stderr_level)
+    logger.info("Starting the exo_oscilloscope pipeline")
 
-    osc = IMUOscilloscope()
-
-    # simulate IMU input
-    t = 0.0
-
-    def fake_data():
-        nonlocal t
-        t += 0.02
-
-        # fake sine wave IMU
-        s1 = np.sin(5 * t)
-        s2 = np.sin(7 * t)
-        s3 = np.sin(9 * t)
-
-        accel = [s1 * 9, s2 * 9, s3 * 9]
-        gyro = [s2 * 180, s3 * 180, s1 * 180]
-        quat = [s1, s2, s3, 0.5]
-
-        # left
-        osc.update_left(accel, gyro, quat)
-
-        # right (phase shifted)
-        osc.update_right(accel, gyro, quat)
-
-    sim_timer = QtCore.QTimer()
-    sim_timer.timeout.connect(fake_data)
-    sim_timer.start(20)
-
-    osc.exit()
+    gui = ExoPlotter()
+    gui.run()
 
 
 if __name__ == "__main__":  # pragma: no cover
