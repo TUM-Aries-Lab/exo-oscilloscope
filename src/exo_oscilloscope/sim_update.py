efe35bef2caafcaeee07b6a0aba63a9d8cb4d92e
tsmorz <tonysmoragiewicz@gmail.com>
2025-11-14 11:46:30 +0100

    add magic method for vector3 multiplication

--- src/exo_oscilloscope/sim_update.py
+++ src/exo_oscilloscope/sim_update.py
@@ -6,6 +6,8 @@ import numpy as np
 
 from exo_oscilloscope.data_classes import IMUData, Quaternion, Vector3
 
+GRAVITY = 9.81
+
 
 def make_simulated_update(gui, start_time: float):
     """Return an update callback that generates fake IMU data.
@@ -18,10 +20,10 @@ def make_simulated_update(gui, start_time: float):
     def update() -> None:
         t = time.time() - start_time
         imu = IMUData(
-            accel=Vector3(t * np.sin(t), np.sin(t * 2), np.sin(t * 4)),
-            gyro=Vector3(np.cos(t), np.cos(t * 2), np.cos(t * 4)),
+            accel=GRAVITY * Vector3(np.sin(t), np.sin(t * 2), np.sin(t * 4)),
+            gyro=180 * Vector3(np.cos(t), np.cos(t * 2), np.cos(t * 4)),
             mag=Vector3(np.cos(t), np.cos(t * 2), np.cos(t * 4)),
-            quat=Quaternion(0.0, 0.0, 0.0, np.sin(t * 8)),
+            quat=Quaternion(np.sin(t + 1), np.sin(t + 2), np.sin(t + 3), np.sin(t + 4)),
             timestamp=t,
         )
         gui.plot_left(imu)
